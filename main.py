020bedb9c5b254abbd9cfdf99d3bf9da0e6c868d
RiMaBo <rimabo@gmail.com>
2025-02-10 19:18:12 -0700

    Switched to Groups for Updating and Drawing

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2,  SCREEN_HEIGHT / 2)
 
     while True:
@@ -17,9 +21,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
